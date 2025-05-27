6e17e0f034ed4970c80d146c8483fddc570d6914
Gilangsetiawan <gilangsetiawan308@gmail.com>
2025-05-27 21:11:09 +0700

    Menambahkan akun pengelola di seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,11 +14,19 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Admin 1',
-            'username' => 'admin',
+            'name' => 'Nisa Fauzziah Habibaloh',
+            'username' => 'nisafauzziah',
             'email' => 'admin@example.com',
-            'password' => Hash::make('password'), 
+            'password' => Hash::make('admin1'), 
             'role' => 'admin', 
+        ]);
+
+        User::create([
+            'name' => 'Pepen Efendi',
+            'username' => 'pepenefendi',
+            'email' => 'pepen@example.com',
+            'password' => Hash::make('pepen123'), 
+            'role' => 'admin',
         ]);
     }
 }
